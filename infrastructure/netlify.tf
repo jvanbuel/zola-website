b429a3002b740cb85a363454a020bae1ccc74e83
Jan <janvanbuel@hotmail.com>
2021-10-28 14:34:39 +0200

    simplify netlify terraform

--- infrastructure/netlify.tf
+++ infrastructure/netlify.tf
@@ -2,8 +2,6 @@ resource "netlify_site" "site" {
   name = "zola-tiltedhat"
 
   repo {
-    command       = "export ZOLA_VERSION=0.14.1;zola build"
-    dir           = "/public"
     provider      = "github"
     repo_path     = "jvanbuel/zola-website"
     repo_branch   = "main"
